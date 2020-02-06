a287f02697d198e3cf1252caa16361eccb80ae36
Leopold Berggreen Nguessan <leopold1@live.dk>
2020-02-06 09:51:46 +0100

    IM GONNA COOOOMITT

---
+++ 1._intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,29 @@
+// Exercise 1 - Console and variables
+
+// ----------------------------------------
+// Variables, strings, numbers, floats
+// ----------------------------------------
+// Exercise 1 - Console and variables
+
+var firstName =  "Leopold";
+var lastName = "Berggreen Nguessan";
+
+console.log("My first name is", firstName, "and my last name is", lastName);
+
+// console.group("My first name" ${firstName})
+// EXERCISE
+// show in the console
+// My first name is Leopold and my last name is Berggreen Nguessan
+
+// Exercise 2  - Numbers and Strings
+
+var year = "2017";
+var number = 1;
+
+// Add the year plus the number
+// The result should be 2018
+// You cannot touch line 1 or 2 
+
+var newYear = +(year) + number
+
+console.log(newYear);
